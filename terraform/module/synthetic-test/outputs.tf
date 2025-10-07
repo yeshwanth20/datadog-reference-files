60938868c1dda2b8fc31b63d552c488c761bf718
yeshwanth20 <120376498+yeshwanth20@users.noreply.github.com>
2025-10-07 15:02:39 +0530

    Create outputs.tf

---
+++ terraform/module/synthetic-test/outputs.tf
@@ -0,0 +1,22 @@
+
+#output "datadog_synthetics_test_names" {
+#  value       = datadog_synthetics_test.synthetic_test[*].name
+#  description = "Names of the created Datadog Synthetic tests"
+#}
+#
+#output "datadog_synthetic_tests" {
+#  value       = datadog_synthetics_test.synthetic_test[*]
+#  description = "The synthetic tests created in DataDog"
+#}
+#
+#output "datadog_synthetic_test_tags" {
+#  value       = datadog_synthetics_test.synthetic_test[*].tags[*]
+#  description = "The synthetic tests created in DataDog"
+#}
+
+output "datadog_synthetic_test_id" {
+    value = {
+        for k , v in datadog_synthetics_test.synthetic_test : k => v.id
+    }
+}
+
